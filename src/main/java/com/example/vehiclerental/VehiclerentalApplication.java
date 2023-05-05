5d60b383aa7d621161359ea2db55212c21d24412
skm <sahil.5@yahoo.com>
2023-05-05 10:48:06 +0530

    vehiclerentalsystem wip 5may2023

--- src/main/java/com/example/vehiclerental/VehiclerentalApplication.java
+++ src/main/java/com/example/vehiclerental/VehiclerentalApplication.java
@@ -1,9 +1,11 @@
 package com.example.vehiclerental;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class VehiclerentalApplication {
 
     public static void main(String[] args) {
